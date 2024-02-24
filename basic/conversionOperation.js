884ee509f0aae21bd35f11d7c59cff3983132b20
Rohit Kumar Yadav <75330588+yadavrky73@users.noreply.github.com>
2024-02-24 08:03:25 +0000

    conversion

---
+++ basic/conversionOperation.js
@@ -0,0 +1,102 @@
+let score = "Rohit"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let phone = 234567
+
+console.log(typeof(phone))
+
+let valueInNumber = Number(score)
+console.log(typeof(valueInNumber));
+
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+
+
+let type = "NA2324"
+let valueConvertInNumber = Number(type)
+console.log(typeof(valueConvertInNumber));
+console.log(valueConvertInNumber);
+
+let typeNum = 45678
+let valueConvertInString = String(typeNum)
+console.log(typeof(valueConvertInString));
+console.log(valueConvertInString);
+
+let typeNull = null
+let valueConvertInNum = Number(typeNull)
+console.log(typeof(valueConvertInNum));
+console.log(valueConvertInNum);
+
+let typeUndefind = true
+let typeUndefindNum = Number(typeUndefind)
+console.log(typeof(typeUndefindNum));
+console.log(typeUndefindNum);
+
+
+let isLoggedIn = ""      //1--true , 0---false.  convert.     empty string ===false
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+//--------operation-----------------
+
+
+let value = -3
+let negValue = --value
+console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+
+let str1 = "hello"
+let str2 = " rohit"
+
+let str3 = str1 + str2
+console.log(str3);
+
+let str4 = "test"
+
+let num = 2
+let con = str4+num
+console.log(con);
+
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter--;
+console.log(--gameCounter);
+console.log(gameCounter--);
+console.log(gameCounter);
+console.log(gameCounter++);
+console.log(gameCounter);
+// link to stud
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
