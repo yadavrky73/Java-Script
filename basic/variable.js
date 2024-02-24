e905a2eb8fbf745ed26e0fe70ea234808a530242
Rohit Kumar Yadav <75330588+yadavrky73@users.noreply.github.com>
2024-02-24 06:16:01 +0000

    variable dec

---
+++ basic/variable.js
@@ -0,0 +1,12 @@
+const accountId = 12221
+
+let accountEmial = "yrohitkumar027@gmail.com"
+
+var accountPassword = "12345678"
+accountCity = "delhi"
+
+//accountId = 23123. //not allowed
+
+console.log(accountId)
+console.log(accountCity)
+console.log(accountPassword)
