24324fdada71b95b4bad134fc27ed845192a28c9
Rohit Kumar Yadav <75330588+yadavrky73@users.noreply.github.com>
2024-02-24 06:30:54 +0000

    variable.js

--- basic/variable.js
+++ basic/variable.js
@@ -5,8 +5,23 @@ let accountEmial = "yrohitkumar027@gmail.com"
 var accountPassword = "12345678"
 accountCity = "delhi"
 
-//accountId = 23123. //not allowed
+let accountState;
 
-console.log(accountId)
-console.log(accountCity)
-console.log(accountPassword)
+//accountId = 23123. //not allowed
+accountEmial ="test@gmail.com"
+accountPassword = "87654321"
+accountCity = "jaipur"
+
+// console.log(accountId)
+// console.log(accountCity)
+// console.log(accountPassword)
+
+//console.log([accountId,accountEmial,accountPassword,accountCity]);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+accountState = "RJ"
+console.table([accountId,accountEmial,accountPassword,accountCity ,accountState]);
