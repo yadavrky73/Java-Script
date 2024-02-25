e2eb42a7271ee8e90700e55e606733428b68f185
Rohit Kumar Yadav <75330588+yadavrky73@users.noreply.github.com>
2024-02-25 05:07:13 +0000

    array_2

--- basic_2/array.js
+++ basic_2/array.js
@@ -15,8 +15,37 @@ myArr.push(7)
 myArr.pop()    //. remove the value back
 //console.log(myArr);
 
-//myArr.unshift(9)          // add the value 0 index/first index  
+myArr.unshift(9)          // add the value 0 index/first index  
 myArr.shift()          // remove the value 0 index/first index  
 myArr.shift() 
 
 console.log(myArr);
+
+
+// array method return the true or false
+
+console.log("method_1", myArr.includes(9));      //return the true or false 
+console.log("method_2", myArr.indexOf(3));
+console.log("method_2", myArr.indexOf(23));      //if that index not in array than return the -1 value
+
+
+//join ---------------------
+
+const newArr = myArr.join()          // join convert the array to string  
+console.log(myArr);                  //return    [ 2, 3, 4, 5, 6 ]
+console.log( newArr);                 //return than one ---2,3,4,5,6
+
+
+// slice, splice.  
+
+console.log("A ", myArr);
+
+const myn1 = myArr.slice(1, 3)      //slice return the array particular range not change the original array
+
+console.log(myn1);
+console.log("B ", myArr);
+
+
+const myn2 = myArr.splice(1, 3)    //slice return the array particular range and change the org array only remaining part is there in array
+console.log("C ", myArr);
+console.log(myn2);
