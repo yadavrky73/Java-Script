20bf8a1051836c5a1d52192e37614579c81ff0b0
Rohit Kumar Yadav <75330588+yadavrky73@users.noreply.github.com>
2024-02-29 12:58:29 +0000

    switch case

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -13,30 +13,30 @@ const temperature = 41
 
 const score = 200
 
-if (score > 100) {
-    let power = "fly"
-    console.log(`User power: ${power}`);
-}
+// if (score > 100) {
+//     let  power = "fly"
+//     console.log(`User power: ${power}`);
+// }
 
-console.log(`User power: ${power}`);
+// console.log(`User power: ${power}`);
 
 
-// const balance = 1000
+const balance = 1000
 
 // if (balance > 500) console.log("test"),console.log("test2");
 
-// if (balance < 500) {
-//     console.log("less than 500");
-// } else if (balance < 750) {
-//     console.log("less than 750");
+if (balance < 500) {
+    console.log("less than 500");
+} else if (balance < 750) {
+    console.log("less than 750");
     
-// } else if (balance < 900) {
-//     console.log("less than 750");
+} else if (balance < 900) {
+    console.log("less than 750");
     
-// } else {
-//     console.log("less than 1200");
+} else {
+    console.log("less than 1200");
 
-// }
+}
 
 const userLoggedIn = true
 const debitCard = true
