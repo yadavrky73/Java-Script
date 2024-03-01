002cdfeee3c520db06b2292301839a2685fd148d
Rohit Kumar Yadav <75330588+yadavrky73@users.noreply.github.com>
2024-03-01 05:19:44 +0000

    while and do while loop

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -5,7 +5,7 @@ for (let i = 0; i <= 10; i++) {
     if (element == 5) {
        // console.log("5 is best number");
     }
-    console.log(element);
+    //console.log(element);
     
 }
 
@@ -23,7 +23,7 @@ let myArray = ["flash", "batman", "superman"]
 //console.log(myArray.length);
 for (let index = 0; index < myArray.length; index++) {
     const element = myArray[index];
-    //console.log(element);
+    console.log(element);
     
 }
 
