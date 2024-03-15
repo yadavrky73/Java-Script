0bddf73e851298edf2f7295d58083542b0e29f53
Rohit Kumar Yadav <75330588+yadavrky73@users.noreply.github.com>
2024-03-15 06:26:01 +0000

    changes

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -13,12 +13,12 @@ const temperature = 41
 
 const score = 200
 
-// if (score > 100) {
-//     let  power = "fly"
-//     console.log(`User power: ${power}`);
-// }
+if (score > 100) {
+    let  power = "fly"
+    console.log(`User power: ${power}`);
+}
 
-// console.log(`User power: ${power}`);
+console.log(`User power: ${power}`);
 
 
 const balance = 1000
--- basic/comparision.js
+++ basic/comparision.js
@@ -1,22 +1,23 @@
-// console.log(2 > 1);
-// console.log(2 >= 1);
-// console.log(2 < 1);
-// console.log(2 == 1);
-// console.log(2 != 1);
+// console.log(2 > 1);       //true
+// console.log(2 >= 1);      //true
+// console.log(2 < 1);       //false
+// console.log(2 == 1);       //false
+// console.log(2 != 1);        //true
 
 
-// console.log("2" > 1);
-// console.log("02" > 1);
+// console.log("2" > 1);         //true
+// console.log("02" > 1);        //true
 
-console.log(null > 0);
-console.log(null == 0);
-console.log(null >= 0);
+// console.log(null > 0);             //false
+// console.log(null == 0);             //false
+// console.log(null >= 0);               //true
 
-console.log(undefined == 0);
-console.log(undefined > 0);
-console.log(undefined < 0);
+// console.log(undefined == 0);           //false
+// console.log(undefined > 0);             //false
+// console.log(undefined < 0);              //false
 
-// === 
-
-console.log("2" === 2);
-console.log("2" === "2");
+// // === 
+ 
+console.log("2" == 2);               //true
+console.log("2" === 2);                   //false
+console.log("2" === "2");                 //true
