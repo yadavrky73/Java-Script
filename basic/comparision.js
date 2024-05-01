85ad962bcadc334dcc63537cbfc462c006123e56
Rohit Kumar Yadav <75330588+yadavrky73@users.noreply.github.com>
2024-05-01 12:57:16 +0000

    add code

--- basic/comparision.js
+++ basic/comparision.js
@@ -12,9 +12,9 @@
 // console.log(null == 0);             //false
 // console.log(null >= 0);               //true
 
-// console.log(undefined == 0);           //false
-// console.log(undefined > 0);             //false
-// console.log(undefined < 0);              //false
+console.log(undefined == 0);           //false
+console.log(undefined > 0);             //false
+console.log(undefined < 0);              //false
 
 // // === 
  
