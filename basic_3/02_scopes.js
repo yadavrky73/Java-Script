c414b80b320c9a5b32e0a83c4de87d7fb51e6945
Rohit Kumar Yadav <75330588+yadavrky73@users.noreply.github.com>
2024-09-12 10:31:44 +0000

    declare the scope global , blog scope

--- basic_3/02_scopes.js
+++ basic_3/02_scopes.js
@@ -16,18 +16,15 @@ if (true) {
 
 function one(){
     const username = "hitesh"
-
     function two(){
         const website = "youtube"
-        console.log(username);
+        console.log(website);
     }
     // console.log(website);
-
      two()
-
 }
 
-// one()
+one()
 
 if (true) {
     const username = "hitesh"
@@ -42,8 +39,18 @@ if (true) {
 
 
 // ++++++++++++++++++ interesting ++++++++++++++++++
+// addone(55)      //in that case we can declare the function before call  
+function addone(num){
+    return num+1
+}
+console.log(addone(55))
 
-
+//addTwo(22, 34);   //in thhat case we can not declare the function before call
+const addTwo = function(num1 , num2){
+    return num1+num2
+}
+let fun = addTwo(22 , 22);
+console.log(fun);
 //var
 
 var y = 300;    // global scope
